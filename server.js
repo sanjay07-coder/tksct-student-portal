3daec883d490e4d067cca22e592a40779c3841bf
sanjay07-coder <sm5439491@gmail.com>
2026-08-08 19:08:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Launchpad is running at http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Launchpad is running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
